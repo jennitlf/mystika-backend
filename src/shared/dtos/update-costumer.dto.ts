b3865f91e3a42312be93b826f063efb555bb2b71
Jennifer <thalitabrasil872@gmail.com>
2025-02-17 16:52:58 -0400

    changing incorrect or unusable data at this point in development

--- src/shared/dtos/update-costumer.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateCostumerDto } from "./create-costumer.dto";
-
-
-export class UpdateCostumerDto extends PartialType(CreateCostumerDto) {};
