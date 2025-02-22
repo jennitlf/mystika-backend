62e10af78c68423e9e6b111ca29b93838e7ad470
Jennifer <thalitabrasil872@gmail.com>
2025-02-22 13:59:36 -0400

    protection of the consultant's agenda creation and deletion routes

--- src/schedule-consultant/schedule-consultant.controller.ts
+++ src/schedule-consultant/schedule-consultant.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { ScheduleConsultantService } from './schedule-consultant.service';
 import { CreateScheduleConsultantDto } from 'src/shared/dtos/create-schedule-consultant.dto';
-import { ApiQuery } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@ApiBearerAuth()
 @Controller('schedule-consultant')
 export class ScheduleConsultantController {
   constructor(private readonly scheduleConsultantService: ScheduleConsultantService) {}
@@ -13,17 +15,16 @@ export class ScheduleConsultantController {
         @Param('idConsultantSpecialty') idConsultantSpecialty: number,
         @Query('date') date?: string,
     ) {
-        return await this.scheduleConsultantService.getTimeslots(
-            idConsultantSpecialty,
-            date, 
-        );
+        return await this.scheduleConsultantService.getTimeslots(idConsultantSpecialty, date);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     create (@Body() createScheduleConsultantDto: CreateScheduleConsultantDto){
         return this.scheduleConsultantService.create(createScheduleConsultantDto)
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     remove (@Param('id') id: string) {
         return this.scheduleConsultantService.remove(id)
