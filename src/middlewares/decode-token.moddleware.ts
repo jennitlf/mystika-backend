21fc02ca1cbdc04fe89dc830f54ff0c3ed068179
Jennifer <thalitabrasil872@gmail.com>
2025-05-13 16:39:25 -0400

    interception of token user data

---
+++ src/middlewares/decode-token.moddleware.ts
@@ -0,0 +1,28 @@
+import { Injectable, NestMiddleware, UnauthorizedException } from "@nestjs/common";
+import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+
+interface RequestWithUser extends Request {
+    user?: any;
+  }
+
+  
+@Injectable()
+export class DecodeTokenMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction){
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')){
+            throw new UnauthorizedException('Token não fornecido ou inválido');
+        }
+        const token = authHeader.split(' ')[1];
+
+        try{
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            (req as RequestWithUser).user = decoded;
+            next();
+        }catch(err){
+            throw new UnauthorizedException('Token invalido ou expirado');
+        }
+    }
+    
+}
