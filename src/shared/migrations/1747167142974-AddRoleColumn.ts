c4426f4925664e664ed32d691a6def9575deb8e9
Jennifer <thalitabrasil872@gmail.com>
2025-05-13 16:51:36 -0400

    correction of the names of the consultant and customer tables in the migration

--- src/shared/migrations/1747167142974-AddRoleColumn.ts
+++ src/shared/migrations/1747167142974-AddRoleColumn.ts
@@ -5,22 +5,22 @@ export class AddRoleColumn1747167142974 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            ALTER TABLE consultants
+            ALTER TABLE consultant
             ADD COLUMN role VARCHAR(255) NOT NULL DEFAULT 'consultant';
         `);
         await queryRunner.query(`
-            ALTER TABLE customers
+            ALTER TABLE customer
             ADD COLUMN role VARCHAR(255) NOT NULL DEFAULT 'customer';
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            ALTER TABLE consultants
+            ALTER TABLE consultant
             DROP COLUMN role;
         `);
         await queryRunner.query(`
-            ALTER TABLE customers
+            ALTER TABLE customer
             DROP COLUMN role;
         `);
     }
