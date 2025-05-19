ad9c9ddbde156a1df0dac59bb9fa4710898cb229
Jennifer Lima <thalitabrasil872@gmail.com>
2025-05-19 00:01:24 -0400

    change: change in the directory, specifically consultation module

--- src/features/consultant/consultation/consultation.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConsultationService } from './consultation.service';
-import { ConsultationController } from './consultation.controller';
-
-@Module({
-  controllers: [ConsultationController],
-  providers: [ConsultationService],
-})
-export class ConsultationModule {}
