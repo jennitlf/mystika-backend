01a31f2506e01c5187933ec8958559c530c497c7
Jennifer Lima <thalitabrasil872@gmail.com>
2025-05-18 23:56:20 -0400

    change: add phone to payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,6 +22,7 @@ export class AuthService {
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
     if (user && (await bcrypt.compare(password, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
     }
@@ -32,6 +33,7 @@ export class AuthService {
     const payload = {
       id: user.id,
       email: user.email,
+      phone: user.phone,
       name: user.name,
       role: user.role,
     };
