25952e20b961cb18fc6dd57ad87372b67f300856
Jennifer <thalitabrasil872@gmail.com>
2025-05-30 13:44:12 -0400

    debug guard error schedule-consultant

--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -10,11 +10,15 @@ export class RoleGuard implements CanActivate {
   constructor(private requiredRoles: string[]) {}
 
   canActivate(context: ExecutionContext): boolean {
+    console.log("estou no role.guard");
     const request = context.switchToHttp().getRequest();
+    console.log("request user:", request.user);
     const user = request.user;
+    console.log("perfil de quem fez a requisição", user.role)
     if (!user || !this.requiredRoles.includes(user.role)) {
       throw new ForbiddenException('Acesso negado: Permissão insuficiente.');
     }
+    console.log("saindo do role.guard")
     return true;
   }
 }
