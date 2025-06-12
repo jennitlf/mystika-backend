726dce4d0e9fc15be2d0721e9751bf1cb211fd12
Jennifer <thalitabrasil872@gmail.com>
2025-06-12 15:19:30 -0400

    remove commits

--- teste.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const user = process.env.POSTGRES_USER;
-const password = process.env.POSTGRES_PASSWORD;
-const dbName = process.env.DATABASE_NAME;
-
-console.log(user, password, dbName);
